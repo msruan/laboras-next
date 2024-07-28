b605f626a7ac7a3c9a923cafcba7b7b65bd700e8
Ruan <santosruan2021@gmail.com>
2024-07-28 20:43:51 -0300

    feat: melhorar profile page

--- src/app/api/posts/profile/[id]/route.ts
+++ src/app/api/posts/profile/[id]/route.ts
@@ -1,16 +1,16 @@
-import { NextApiRequest } from 'next';
-import { NextResponse } from 'next/server';
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
 
-import { connectToDb } from '@/lib/utils';
-import { Post } from '@/models/posts';
+import { connectToDb } from "@/lib/utils";
+import { Post } from "@/models/posts";
 
 export const GET = async (request: NextApiRequest, { params }: any) => {
   try {
     await connectToDb();
     const { id } = params;
 
-    const post = await (await Post()).find({ user_id: id });
-    return NextResponse.json(post);
+    const posts = await (await Post()).find({ user_id: id });
+    return NextResponse.json(posts);
   } catch (err) {
     console.log(err);
     return NextResponse.error();
