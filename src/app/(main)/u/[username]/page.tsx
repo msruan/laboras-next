c8b00f3577d6bce55c674f6895657ed8ef1b9d3c
Ruan <santosruan2021@gmail.com>
2024-07-28 20:43:51 -0300

    feat: melhorar profile page

--- src/app/(main)/u/[username]/page.tsx
+++ src/app/(main)/u/[username]/page.tsx
@@ -1,12 +1,12 @@
-import { FC } from 'react';
+import { FC } from "react";
 
-import { Header } from '@/components/Header';
-import { PostsContainer } from '@/components/PostsContainer';
-import { Profile } from '@/components/profile/Profile';
-import { api } from '@/config/api';
-import { auth } from '@/lib/auth';
-import { IPost } from '@/models/posts';
-import { IProfile } from '@/models/profiles';
+import { Header } from "@/components/Header";
+import { PostsContainer } from "@/components/PostsContainer";
+import { Profile } from "@/components/profile/Profile";
+import { api } from "@/config/api";
+import { auth } from "@/lib/auth";
+import { IPost } from "@/models/posts";
+import { IProfile } from "@/models/profiles";
 
 type Props = {
   params: {
@@ -26,7 +26,11 @@ const UserPage: FC<Props> = async ({ params }) => {
     <div className="flex flex-col h-full max-xl:border-0 gap-2 pl-3 pr-3 border-rebeccapurple2 border-r-2 border-l-2">
       <div className="flex flex-col gap-1">
         <Header title={profile.username} />
-        <Profile profile={profile!} email={session?.user?.email!} />
+        <Profile
+          postsCount={posts.length}
+          profile={profile!}
+          email={session?.user?.email!}
+        />
       </div>
       <PostsContainer textbox={false} posts={posts} />
     </div>
