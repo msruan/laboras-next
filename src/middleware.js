3c7a9121251b3ffade4771194d4828874a5cb2b4
RavCarvalho <ravennacarvalho098@gmail.com>
2024-08-20 14:18:46 -0300

    feat: melhorar botao de logout

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,10 @@
 import { auth } from "@/lib/auth";
 
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/sign") {
-    const newUrl = new URL("/sign", process.env.SERVER_URL);
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
+    const newUrl = new URL("/login", process.env.SERVER_URL);
     return Response.redirect(newUrl);
-  } else if (req.auth && req.nextUrl.pathname === "/sign") {
+  } else if (req.auth && req.nextUrl.pathname === "/login") {
     const newUrl = new URL("/", process.env.SERVER_URL);
     return Response.redirect(newUrl);
   }
